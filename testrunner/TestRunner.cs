43d98d98773e244c4d5f5720d45b39d49b4bf64c
hsnakd <hsnakd10@gmail.com>
2024-08-25 13:22:16 +0300

    804

--- testrunner/TestRunner.cs
+++ testrunner/TestRunner.cs
@@ -1,21 +1,23 @@
-using NUnit.Framework;
-using TechTalk.SpecFlow;
+package testrunner;
 
-namespace Miaplaza.TestRunner
-{
-    [SetUpFixture]
-    public class TestRunner
-    {
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            // Setup code if needed
-        }
 
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            // Teardown code if needed
-        }
-    }
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        // Rerun failed tests from rerun.txt file
+        features = {"src/test/resources/features/"},
+        glue = {"stepdefinitions", "hooks"},
+        plugin = {"pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/rerun.txt"  // Save Failed test scenarios in rerun.txt file
+        },
+        dryRun = false,
+        tags = "",
+        publish = true
+)
+
+public class TestRunner {
 }
